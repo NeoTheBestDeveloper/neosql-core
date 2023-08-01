4585bdf67960c19ca573d27db65f02af4c4f850f
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2023-08-01 09:52:11 +0300

    refactor: refactor page

--- src/database_driver/page.c
+++ src/database_driver/page.c
@@ -8,23 +8,21 @@
 #include "database_driver/page.h"
 #include "utils/stream_ext.h"
 
-static inline i64 page_calc_offset(i32 page_id)
-{
-    return HEADER_SIZE + page_id * PAGE_SIZE;
-}
+// PRIVATE METHODS SIGNATURE START.
+static inline i64 page_calc_offset(i32 page_id);
+static void seek_to_page(i32 fd, i32 page_id);
+static void write_page_buf(const u8* buf, i32 page_id, i32 fd);
+static u8* read_page_buf(i32 page_id, i32 fd);
+static Page page_deserialise(u8* buf);
+static u8* page_serialise(const Page* page);
+// PRIVATE METHODS SIGNATURE END.
 
+// PUBLIC METHODS START.
 Page page_new(i32 page_id, i32 fd)
 {
-    i64 offset = page_calc_offset(page_id);
-    lseek(fd, offset, SEEK_SET);
+    u8* serialized_page_buf = read_page_buf(page_id, fd);
 
-    u8* serialized_page_buf = malloc(PAGE_SIZE);
-    read(fd, serialized_page_buf, PAGE_SIZE);
-
-    Stream stream
-        = stream_new(serialized_page_buf, PAGE_SIZE, DEFAULT_DATABASE_ENDIAN);
-
-    Page page = stream_read_page(&stream);
+    Page page = page_deserialise(serialized_page_buf);
     page.id = page_id;
 
     return page;
@@ -32,16 +30,52 @@ Page page_new(i32 page_id, i32 fd)
 
 void page_write(const Page* page, i32 fd)
 {
-    i64 offset = page_calc_offset(page->id);
-    lseek(fd, offset, SEEK_SET);
-
-    u8* serialized_page_buf = malloc(PAGE_SIZE);
-    Stream stream
-        = stream_new(serialized_page_buf, PAGE_SIZE, DEFAULT_DATABASE_ENDIAN);
-    stream_write_page(&stream, page);
-
-    write(fd, serialized_page_buf, PAGE_SIZE);
+    u8* serialized_page_buf = page_serialise(page);
+    write_page_buf(serialized_page_buf, page->id, fd);
     free(serialized_page_buf);
 }
 
 void page_free(Page* page) { free(page->payload - PAGE_HEADER_SIZE); }
+// PUBLIC METHODS END.
+
+static inline i64 page_calc_offset(i32 page_id)
+{
+    return HEADER_SIZE + page_id * PAGE_SIZE;
+}
+
+static void seek_to_page(i32 fd, i32 page_id)
+{
+    i64 offset = page_calc_offset(page_id);
+    lseek(fd, offset, SEEK_SET);
+}
+
+static void write_page_buf(const u8* buf, i32 page_id, i32 fd)
+{
+    seek_to_page(fd, page_id);
+    write(fd, buf, PAGE_SIZE);
+}
+
+static u8* read_page_buf(i32 page_id, i32 fd)
+{
+    u8* buf = malloc(PAGE_SIZE);
+
+    seek_to_page(fd, page_id);
+    read(fd, buf, PAGE_SIZE);
+
+    return buf;
+}
+
+static Page page_deserialise(u8* buf)
+{
+    Stream stream = stream_new(buf, PAGE_SIZE, DEFAULT_DATABASE_ENDIAN);
+    return stream_read_page(&stream);
+}
+
+// Convert page to bytes which allocated on heap.
+static u8* page_serialise(const Page* page)
+{
+    u8* buf = malloc(PAGE_SIZE);
+    Stream stream = stream_new(buf, PAGE_SIZE, DEFAULT_DATABASE_ENDIAN);
+    stream_write_page(&stream, page);
+    return buf;
+}
